787b9ae6be001c0a05358d738b64edfe44b45f7d
Rajan Singhal <mr.rajansinghal@gmail.com>
2017-07-12 19:16:35 +0530

    initial commit resourse loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core'; 
-import {PaginatePipe, PaginationService} from 'ng2-pagination';  
+import { IProduct } from './products'; 
+import { ProductService } from './products.service'; 
 
-@Component({ 
-   selector: 'form-app', 
-   templateUrl: './app.component.html'     
-}) 
-export class AppComponent { 
-   
+import { Http , Response } from '@angular/http'; 
+import { Observable } from 'rxjs/Observable'; 
+import 'rxjs/add/operator/map';  
+
+@Component ({ 
+   selector: 'form-app',  
+   template: '<div>Hello</div>', 
+   providers: [ProductService] 
+})  
+
+export   class   AppComponent  {  
+   iproducts: IProduct[]; 
+	constructor(private _product: ProductService){ 
+   } 
+   ngOnInit() : void { 
+      this._product.getproducts() 
+      .subscribe(iproducts => this.iproducts = iproducts); 
+   } 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule }      from '@angular/core'; 
-import { BrowserModule } from '@angular/platform-browser';  
-
+import { BrowserModule } from '@angular/platform-browser'; 
 import { AppComponent }  from './app.component';  
-import { MultiplierPipe } from './multiplier.pipe'  
 
-
+import { HttpModule } from '@angular/http'; 
 @NgModule({ 
-   imports:      [ BrowserModule], 
-   declarations: [ AppComponent, MultiplierPipe], 
+   imports:      [ BrowserModule,HttpModule], 
+   declarations: [ AppComponent], 
    bootstrap:    [ AppComponent ] 
 }) 
 export class AppModule { }
